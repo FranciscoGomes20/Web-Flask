b5b115f2966b1e63b32477538a8f8016cc39f4ba
FranciscoGomes20 <henriquecgomes10@gmail.com>
2021-06-07 22:06:08 -0500

    Adicionado uma session e flash

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from os import name
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -18,16 +18,17 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Parece que você mudou de nome! ')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
-
     return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 @app.errorhandler(404)
